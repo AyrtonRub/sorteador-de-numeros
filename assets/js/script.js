75d2655573f241f15ebdb30a9643b1f6743386d0
Ayrton Rubens <129620321+AyrtonRub@users.noreply.github.com>
2025-06-02 22:15:46 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@
   const configDraw = document.getElementById("configDraw");
   const inputsNumb = document.querySelectorAll("[data-input]");
   const toggle = document.getElementById("toggle");
-  const output = document.getElementById('output')
+  const output = document.getElementById("output");
+  const result = document.querySelector("#resultado");
+  const resultOriginal = result.innerHTML;
 
   //Função para não permitir letras e números nos inputs de números
   inputsNumb.forEach((input) => {
@@ -18,12 +20,12 @@
 
   function random(min, max) {
     if (min > max) {
-      alert(
+      return alert(
         `O número minimo ${min} é maior que máximo ${max}, por favor, digite novamente os dados`
       );
     }
 
-    return Math.floor(Math.random() * (max - min)) +  min;
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
   //função para criar tags
@@ -47,23 +49,23 @@
     const qtdNumbers = document.getElementById("numbers").value;
     const min = document.getElementById("min").value;
     const max = document.getElementById("max").value;
+    const appearAnimation = document.querySelector("[data-appearAnimation]");
 
     if (!qtdNumbers || !min || !max) {
       return alert(`Por favor, insira todos os números antes de continuar`);
     }
 
-    const parseQtdNumbers = parseInt(qtdNumbers)
-    const parseMin = parseInt(min)
-    const parseMax = parseInt(max)
+    const parseQtdNumbers = parseInt(qtdNumbers);
+    const parseMin = parseInt(min);
+    const parseMax = parseInt(max);
 
-    if (parseQtdNumbers > max - parseMin + 1) {
+    if (parseQtdNumbers > parseMax - parseMin + 1) {
       return alert(
         `a quantidde de números ${parseQtdNumbers} é maior que o range entre min e max, por favor realize o ajuste para continuar`
       );
     }
 
     const enabled = toggle.checked;
-    arrNumbersDraw.length = 0
 
     while (arrNumbersDraw.length < parseQtdNumbers) {
       const numSort = random(parseMin, parseMax);
@@ -76,55 +78,76 @@
       }
     }
 
-    btndisplayNone.classList.toggle('display-none')
-    btnDrawAgain.classList.toggle('display-none')
-    configDraw.classList.toggle('display-none')
+    btndisplayNone.classList.toggle("display-none");
+    configDraw.classList.toggle("display-none");
+
+     let contador = 0
+    
+    //Fech para carregar html
+    fetch("teste.html")
+      .then((response) => response.text())
+      .then((html) => {
+        result.innerHTML = html;
+        
+        const contResult = document.getElementById('contResult')
+         contador++
+        contResult.textContent = `${contador}° resultado`
+      });
+
+      console.log(contador)
+
+     //função para contar a quantidade de resultados 
+   
 
     //função para a criação dos números na tela
     function createItem(num) {
-      const span = createTag('span')
-      const p = createTag('p', 'draw-number')
-      p.textContent = num
-      span.appendChild(p)
+      const span = createTag("span");
+      span.setAttribute("data-appearAnimation", "");
+      const p = createTag("p", "draw-number");
+      p.textContent = num;
+      span.appendChild(p);
 
-      return span
+      return span;
     }
 
+    const arr = [];
 
+    arrNumbersDraw.forEach((el) => {
+      arr.push(createItem(el));
+    });
 
-    function eventAnimation (item) {
-      return new Promise (resolve => {
-        item.addEventListener('animationend', function() {
-          resolve()
-        })
-      })
+    function eventAnimation(item) {
+      return new Promise((resolve) => {
+        item.addEventListener("animationend", function () {
+          resolve();
+        });
+      });
     }
 
-    async function waiForAnimation() {
+    async function waitForEvent() {
       try {
-        for(let i in arrNumbersDraw) {
-          const numArr = arrNumbersDraw[i]
-          const item = createItem(numArr)
-          output.appendChild(item)
-          await eventAnimation(item)
+        for (let i of arr) {
+          const item = i;
+          output.appendChild(item);
+          await eventAnimation(item);
         }
-      } catch(err) {
-        throw new Error(err)
+        appearAnimation.classList.add("appearButton");
+      } catch (err) {
+        throw new Error(err);
       }
     }
 
-    waiForAnimation()
-
+    waitForEvent();
   }
-
   btnDraw.addEventListener("click", drawNumbers);
 
-  btnDrawAgain.addEventListener('click', function(e) {
-    e.preventDefault()
-    output.textContent = ''
-
-    btndisplayNone.classList.toggle('display-none')
-    btnDrawAgain.classList.toggle('display-none')
-    configDraw.classList.toggle('display-none')
-  })
+  btnDrawAgain.addEventListener("click", function (e) {
+    e.preventDefault();
+    this.classList.remove("appearButton");
+    result.innerHTML = resultOriginal;
+    output.textContent = "";
+    arrNumbersDraw.length = 0;
+    btndisplayNone.classList.toggle("display-none");
+    configDraw.classList.toggle("display-none");
+  });
 })();
